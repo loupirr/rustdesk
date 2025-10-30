e1f7e35ae9b50ac712fc5d356512b9e6619ad542
loupirr <93262617+loupirr@users.noreply.github.com>
2025-10-30 17:26:56 +0100

    Add Windows installer batch script for RustDesk

---
+++ install_complete/windows/installer_rustdesk_win.bat
@@ -0,0 +1,20 @@
+@echo off
+
+
+set "url=https://raw.githubusercontent.com/loupirr/rustdesk/refs/heads/main/install_complete/windows/client_rustdesk_windows.ps1"
+set "local=C:\Temp\test.ps1"
+
+REM Crée C:\Temp s'il n'existe pas
+if not exist "C:\Temp" (
+    echo Creation du dossier C:\Temp...
+    mkdir "C:\Temp"
+)
+
+echo Downloading %url% to %local%...
+powershell -NoProfile -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%local%' -UseBasicParsing"
+
+echo Executing the downloaded script...
+powershell -NoProfile -ExecutionPolicy Bypass -File "%local%"
+
+pause
+
